61e3911d0d6b4528ee478cec8b830c7ba277c7f7
Dibs <93832725+hunter-dibs@users.noreply.github.com>
2022-12-11 03:19:15 +0600

    Update dashmessagecount.php

--- dashmessagecount.php
+++ dashmessagecount.php
@@ -5,6 +5,3 @@ $conn = mysqli_connect('localhost', 'root', '', 'donation_plus');
 if (!$conn) {
     die("Connection failed");
 }
-$sql = "SELECT * FROM contact";
-$query = $conn->query($sql);
-echo "<h1>" . $query->num_rows . "</h1>";
